a51a2652cde9fa34798bcc55d89c5712b33f5d90
Choetsu <samy.amh94@gmail.com>
2021-09-08 22:41:15 +0200

    ajout des vues et bd

---
+++ app/verif_formulaire.php
@@ -0,0 +1,29 @@
+<?php
+  $host = "localhost"; // ou 127.0.0.1
+  $user = "root";  // "admin" pour Linux
+  $bdd = "BD_inscription"; // le nom de votre base de données
+  $passwd = "";  // "admin" pour Linux
+  $co = mysqli_connect($host , $user , $passwd, $bdd) or die("erreur de connexion");
+  
+   
+   $prenom=$_POST["prenom"];
+   $nom=$_POST["nom"];
+   $email=$_POST["email"];
+   $mdp=$_POST["mdp"];
+
+       if(!preg_match("#^[0-9][0-9][a-zA-Z]{6}$#",$mdp)){
+        echo 'Erreur : le mot de passe est non valide (2 chiffres suivant 6 caractères)';}
+      elseif(!preg_match("#^[a-zA-Z0-9]+@[a-z]+(\.)+[a-z]{2,3}#",$email)){
+        echo 'Erreur : email non valide';}
+
+      if(empty($prenom) && empty($nom) && empty($email) && empty($mdp)){
+         echo 'Un des champs est vide';
+      }
+    else{
+      /*elseif(!preg_match("#[a-zA-Z0-9]#",$user)){
+        echo 'Erreur : Les caractères utilisable sont : a-z A-Z 0-9 pour les users';}*/
+       
+ $sql = "INSERT INTO user(prenom,nom,email,passwrd) VALUES('$prenom','$nom','$email','$mdp')";
+        $co->query($sql);
+      header('Location: formulaire.html');
+}
