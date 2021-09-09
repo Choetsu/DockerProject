bc1087c53d0500d777e9b6c3997deab1de56eabc
Choetsu <samy.amh94@gmail.com>
2021-09-09 09:51:28 +0200

    new update

--- app/verif_formulaire.php
+++ app/verif_formulaire.php
@@ -6,24 +6,23 @@
   $co = mysqli_connect($host , $user , $passwd, $bdd) or die("erreur de connexion");
   
    
-   $prenom=$_POST["prenom"];
-   $nom=$_POST["nom"];
-   $email=$_POST["email"];
-   $mdp=$_POST["mdp"];
+  $prenom=$_POST["prenom"];
+  $nom=$_POST["nom"];
+  $email=$_POST["email"];
+  $mdp=$_POST["mdp"];
 
-       if(!preg_match("#^[0-9][0-9][a-zA-Z]{6}$#",$mdp)){
-        echo 'Erreur : le mot de passe est non valide (2 chiffres suivant 6 caractères)';}
-      elseif(!preg_match("#^[a-zA-Z0-9]+@[a-z]+(\.)+[a-z]{2,3}#",$email)){
-        echo 'Erreur : email non valide';}
-
-      if(empty($prenom) && empty($nom) && empty($email) && empty($mdp)){
-         echo 'Un des champs est vide';
-      }
-    else{
-      /*elseif(!preg_match("#[a-zA-Z0-9]#",$user)){
-        echo 'Erreur : Les caractères utilisable sont : a-z A-Z 0-9 pour les users';}*/
-       
- $sql = "INSERT INTO user(prenom,nom,email,passwrd) VALUES('$prenom','$nom','$email','$mdp')";
-        $co->query($sql);
-      header('Location: formulaire.html');
-}
+  if(!preg_match("#^[0-9][0-9][a-zA-Z]{6}$#",$mdp)){
+    echo 'Erreur : le mot de passe est non valide (2 chiffres suivant 6 caractères)';
+  }
+  elseif(!preg_match("#^[a-zA-Z0-9]+@[a-z]+(\.)+[a-z]{2,3}#",$email)){
+    echo 'Erreur : email non valide';
+  }
+  elseif(empty($prenom) && empty($nom) && empty($email) && empty($mdp)){
+    echo 'Un des champs est vide';
+  }
+  else{
+    $sql = "INSERT INTO user(prenom,nom,email,passwrd) VALUES('$prenom','$nom','$email','$mdp')";
+    $co->query($sql);
+    header('Location: formulaire.html');
+  }
+?>
